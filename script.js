76e89e2962acfd08883033948c08076c36335240
Nadia Pandoulis <z5358911@ad.unsw.edu.au>
2024-04-18 16:40:45 +1000

    Clean up

--- script.js
+++ script.js
@@ -355,43 +355,4 @@ function shuffleArray(array) {
     return array;
 }
 
-
-//function mergeRemainingButtons() {
-//    var remainingButtons = document.querySelectorAll('.button-grid button:not(.theme-button)');
-//    var remainingPairs = wordPairs.filter(pair => !successfulPairs.includes(pair));
-
-//    remainingButtons.forEach(remainingButton => {
-//        var word = remainingButton.textContent;
-//        remainingPairs.forEach(wordPair => {
-//            if (wordPair.associatedWords.includes(word)) {
-//                var mergeButton = createMergeButton(wordPair.theme, wordPair.color, wordPair.associatedWords);
-//                var parentRow = remainingButton.parentNode;
-//                parentRow.replaceChild(mergeButton, remainingButton);
-//                remainingPairs.splice(remainingPairs.indexOf(wordPair), 1); // Remove the pair from the remaining pairs array
-//            }
-//        });
-//    });
-
-//    // Remove any remaining non-merge buttons
-//    remainingButtons.forEach(remainingButton => {
-//        remainingButton.parentNode.removeChild(remainingButton);
-//    });
-
-//    /*removeExcessButtons();*/
-//}
-
-////function removeExcessButtons() {
-////    var remainingButtons = document.querySelectorAll('.button-grid button:not(.theme-button)');
-////    var numRows = Math.ceil(remainingButtons.length / 4);
-
-////    // Remove excess buttons if the number of rows exceeds 4
-////    if (numRows > 4) {
-////        for (let i = 4; i < numRows; i++) {
-////            var excessButtons = document.querySelectorAll('.button-grid button:not(.theme-button):nth-last-child(-n+' + (remainingButtons.length - (i - 1) * 4) + ')');
-////            excessButtons.forEach(button => button.parentNode.removeChild(button));
-////        }
-////    }
-//}
-
-
 initializeGame();
